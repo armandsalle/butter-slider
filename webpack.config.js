055caf945a8d8d5cd058b2fa57cd0d689c52e7c0
Armand Sallé <armand.salle@gmail.com>
2020-05-18 13:02:20 +0200

    👨‍🏭Add Webpack for build

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'slider',
+    libraryTarget: 'var',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+}
+
+module.exports = config
