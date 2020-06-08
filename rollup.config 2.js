500e1e278d9bae65dbbeb0855eff613480531232
Armand Sallé <armand.salle@gmail.com>
2020-06-08 22:16:34 +0200

    Remove old doc

---
+++ rollup.config 2.js
@@ -0,0 +1,33 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+
+const DIST = 'dist'
+const BUNDLE = 'bundle'
+const PRODUCTION = !process.env.ROLLUP_WATCH
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: `${DIST}/${BUNDLE}.cjs.js`,
+      format: 'cjs',
+    },
+    {
+      file: `${DIST}/${BUNDLE}.esm.js`,
+      format: 'esm',
+    },
+    {
+      name: 'slider',
+      file: `${DIST}/${BUNDLE}.umd.js`,
+      format: 'umd',
+    },
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    PRODUCTION && terser(),
+  ],
+}
